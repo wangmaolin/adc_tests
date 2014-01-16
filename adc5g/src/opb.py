d64fd5e179b6f9e3929c40b4e07cb8d9d665672d
jack <jack@appcl.(none)>
2014-01-16 17:07:17 +0000

    Added method for changing IODELAY delays

--- adc5g/src/opb.py
+++ adc5g/src/opb.py
@@ -15,3 +15,15 @@ def inc_mmcm_phase(roach, zdok_n, inc=1):
     """
     reg_val = pack(OPB_DATA_FMT, (1<<(zdok_n*4)) + (inc<<(1+zdok_n*4)), 0x0, 0x0)
     roach.blindwrite(OPB_CONTROLLER, reg_val, offset=0x0)
+
+def set_io_delay(r,zdok,core,delay,bit='all',regname='adc5g_controller'):
+    ADC_BITS = 8
+    if bit == 'all':
+        bit_range = range(ADC_BITS)
+    else:
+        bit_range = [bit]
+    for i in bit_range:
+        data_pin = (core<<3) + i
+        reg_val = (delay<<24) + (data_pin<<16) + 0x01
+        reg_val_str = pack('>L',reg_val)
+        r.blindwrite(regname,reg_val_str,offset=((4+zdok)*4))
